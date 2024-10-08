92b9c6f88ef57858f88fd70407c948cc0e2313df
Sergey Prokudin <sergey.prokudin@gmail.com>
2024-10-08 16:26:14 +0200

    setup.py typo fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     description='Effortlessly gather, trace, and manage citations for academic papers',
     author='Sergey Prokudin',
-    author_email='sergey.prokudin@gmail.com,
+    author_email='sergey.prokudin@gmail.com',
     url='https://github.com/sergeyprokudin/scholtrack',
     packages=find_packages(),
     install_requires=[
@@ -16,6 +16,12 @@ setup(
         'console_scripts': [
             'scholtrack=scholtrack.cli:main',
         ],
-    }
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
 
