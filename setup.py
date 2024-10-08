a4084931b4a741db3844f4f5388b5b61ba5b8473
Sergey Prokudin <sergey.prokudin@gmail.com>
2024-10-08 14:54:57 +0200

    Apache license updated

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='scholtrack',
     version='0.1.0',
-    description='Effortlessly gather, trace, and manage citations for academic papers.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    description='Effortlessly gather, trace, and manage citations for academic papers',
+    author='Sergey Prokudin',
+    author_email='sergey.prokudin@gmail.com,
     url='https://github.com/sergeyprokudin/scholtrack',
     packages=find_packages(),
     include_package_data=True,
@@ -20,7 +20,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache 2.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
